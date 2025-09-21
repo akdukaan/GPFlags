e7134441f2248df8b1cc906f9b6e193e09ca7615
Kaan <drbot7@gmail.com>
2025-09-21 13:03:56 -0500

    Delay all of PlayerToggleFlightEvent by 1 tick

--- src/main/java/me/ryanhamshire/GPFlags/FlightManager.java
+++ src/main/java/me/ryanhamshire/GPFlags/FlightManager.java
@@ -211,23 +211,26 @@ public class FlightManager implements Listener {
     @EventHandler
     private void onFlyToggle(PlayerToggleFlightEvent event) {
         Player player = event.getPlayer();
-        Location location = player.getLocation();
-        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(location, false, null);
-        boolean manageFlight = gpfManagesFlight(player);
-        if (!manageFlight) return;
-        
-        if (FlagDef_OwnerMemberFly.letPlayerFly(player, location, claim)) {
-            turnOnFlight(player);
-            return;
-        }
-        if (FlagDef_OwnerFly.letPlayerFly(player, location, claim)) {
-            turnOnFlight(player);
-            return;
-        }
-        
-        if (!FlagDef_NoFlight.letPlayerFly(player, location, claim)) {
-            turnOffFlight(player);
-        }
+        Bukkit.getScheduler().runTaskLater(GPFlags.getInstance(), () -> {
+            Location location = player.getLocation();
+            Claim claim = GriefPrevention.instance.dataStore.getClaimAt(location, false, null);
+            boolean manageFlight = gpfManagesFlight(player);
+            if (!manageFlight) return;
+
+            if (FlagDef_OwnerMemberFly.letPlayerFly(player, location, claim)) {
+                turnOnFlight(player);
+                return;
+            }
+            if (FlagDef_OwnerFly.letPlayerFly(player, location, claim)) {
+                turnOnFlight(player);
+                return;
+            }
+
+            if (!FlagDef_NoFlight.letPlayerFly(player, location, claim)) {
+                turnOffFlight(player);
+            }
+        }, 1);
+
     }
 
     private static void turnOffFlight(@NotNull Player player) {
