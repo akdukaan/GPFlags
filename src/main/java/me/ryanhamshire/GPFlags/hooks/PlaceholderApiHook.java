0051b1f543397fd6428cc39ee45939cf09e7ca1b
FakE <68513641+FakEEE14@users.noreply.github.com>
2025-10-16 12:32:07 +0330

    Update PlaceholderApiHook.java
    added flagparam so you can get playertime, enter/exit message and other things

--- src/main/java/me/ryanhamshire/GPFlags/hooks/PlaceholderApiHook.java
+++ src/main/java/me/ryanhamshire/GPFlags/hooks/PlaceholderApiHook.java
@@ -12,11 +12,8 @@ import me.ryanhamshire.GPFlags.util.Util;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import me.ryanhamshire.GriefPrevention.PlayerData;
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -60,47 +57,57 @@ public class PlaceholderApiHook extends PlaceholderExpansion {
 
     @Override
     public @Nullable String onRequest(OfflinePlayer offlinePlayer, @NotNull String identifier) {
-        identifier = identifier.toLowerCase();
         if (!(offlinePlayer instanceof Player)) return null;
         Player player = (Player) offlinePlayer;
-        String flagName = identifier.substring(identifier.indexOf('_') + 1);
+        identifier = identifier.toLowerCase();
 
-        if (identifier.startsWith("cansetclaimflag_")) {
-            // Check perms for that specific flag
-            if (!player.hasPermission("gpflags.flag." + flagName)) {
-                MessagingUtil.sendMessage(player, TextMode.Err, Messages.NoFlagPermission, flagName);
-                return "No";
-            }
+        String[] parts = identifier.split("_", 2);
+        if (parts.length < 2) return null;
 
-            // Check that the flag can be used in claims
-            FlagDefinition def = plugin.getFlagManager().getFlagDefinitionByName(flagName);
-            if (!def.getFlagType().contains(FlagDefinition.FlagType.CLAIM)) {
-                MessagingUtil.sendMessage(player, TextMode.Err, Messages.NoFlagInClaim);
-                return "No";
-            }
+        String type = parts[0];
+        String flagName = parts[1];
 
-            // Check that they are standing in a claim
-            PlayerData playerData = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId());
-            Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), false, playerData.lastClaim);
-            if (claim == null) {
-                return "No";
-            }
+        PlayerData playerData = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId());
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), false, playerData.lastClaim);
 
-            // Check that they can set flags in the area
-            if (!Util.canEdit(player, claim)) {
-                MessagingUtil.sendMessage(player, TextMode.Err, Messages.NotYourClaim);
-                return "No";
-            }
+        switch (type) {
+            case "cansetclaimflag":
+                // Check perms for that specific flag
+                if (!player.hasPermission("gpflags.flag." + flagName)) {
+                    MessagingUtil.sendMessage(player, TextMode.Err, Messages.NoFlagPermission, flagName);
+                    return "No";
+                }
 
-            return "Yes";
+                // Check if flag exists and is usable in claims
+                FlagDefinition def = plugin.getFlagManager().getFlagDefinitionByName(flagName);
+                if (def == null || !def.getFlagType().contains(FlagDefinition.FlagType.CLAIM)) {
+                    MessagingUtil.sendMessage(player, TextMode.Err, Messages.NoFlagInClaim);
+                    return "No";
+                }
+
+                // Check that they are standing in a claim
+                if (claim == null) return "No";
+
+                // Check that they can set flags in the area
+                if (!Util.canEdit(player, claim)) {
+                    MessagingUtil.sendMessage(player, TextMode.Err, Messages.NotYourClaim);
+                    return "No";
+                }
+
+                return "Yes";
+
+            case "isflagactive":
+                if (claim == null) return "No";
+                Flag flagActive = plugin.getFlagManager().getEffectiveFlag(player.getLocation(), flagName, claim);
+                return (flagActive != null) ? "Yes" : "No";
+
+            case "flagparam":
+                if (claim == null) return "No";
+                Flag flagParam = plugin.getFlagManager().getEffectiveFlag(player.getLocation(), flagName, claim);
+                return (flagParam != null) ? flagParam.parameters : "";
+
+            default:
+                return null;
         }
-        if (identifier.startsWith("isflagactive_")) {
-            PlayerData playerData = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId());
-            Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), false, playerData.lastClaim);
-            Flag flag = plugin.getFlagManager().getEffectiveFlag(player.getLocation(), flagName, claim);
-            if (flag == null) return "No";
-            return "Yes";
-        }
-        return null;
     }
 }
